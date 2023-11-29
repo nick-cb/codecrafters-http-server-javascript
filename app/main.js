99e6de605d787179784c7d102da80abf776d2063
VI Nguyen <nguyenhungvicb@gmail.com>
2023-11-30 05:12:45 +0700

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -2,6 +2,10 @@ const net = require("net");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on('data', (_) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    socket.end();
+  })
   socket.on("close", () => {
     socket.end();
     server.close();
