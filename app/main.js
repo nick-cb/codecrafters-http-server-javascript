e3a8cb14c63f24e61f291b87b77c3c143a696f68
VI Nguyen <nguyenhungvicb@gmail.com>
2024-01-01 20:29:47 +0700

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -1,11 +1,28 @@
 const net = require("net");
 
+const METHODS = ["GET", "POST", "PUT", "DELETE"];
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on('data', (_) => {
+  socket.on("data", (data) => {
+    const requestRaw = data.toString();
+    // console.log(request);
+    const lines = requestRaw.trim().split("\r\n");
+    const request = lines.reduce((prev, line) => {
+      const spaceIdx = line.indexOf(" ");
+      const [key, value] = [line.slice(0, spaceIdx), line.slice(spaceIdx + 1)];
+      return { ...prev, [key?.replaceAll(": ", "")]: value };
+    }, {});
+    for (const method of METHODS) {
+      const [path, _] = request[method]?.split(" ");
+      if (path && path !== "/") {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        socket.end();
+        return;
+      }
+    }
     socket.write("HTTP/1.1 200 OK\r\n\r\n");
     socket.end();
-  })
+  });
   socket.on("close", () => {
     socket.end();
     server.close();
